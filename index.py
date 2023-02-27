b838e9e9b5d0e884dfed4cc2784e28fd167e88e4
Luiz Esquivel <62815987+luizesquivel05@users.noreply.github.com>
2023-02-27 20:05:53 -0300

    criacao da leitura de usuarios do jogo

--- index.py
+++ index.py
@@ -27,6 +27,14 @@ botao_competitivo.place(x=210, y=320)
 
 # define a função para abrir a tela de login
 def abrir_tela_login(tipo_jogo):
+    
+# define a função para realizar o login
+    def dadosUSER(p1, p2, p3, p4, p5, p6, tipo_jogo):
+        # implemente sua lógica de login aqui
+        # ...
+        dadosJOGO = {'p1': f'{p1}','p2': f'{p2}','p3': f'{p3}','p4': f'{p4}','p5': f'{p5}','p6': f'{p6}','tipo_jogo': f'{tipo_jogo}',}
+        return dadosJOGO
+    
     janela_principal.destroy()
     
     # cria a janela de login
@@ -36,26 +44,34 @@ def abrir_tela_login(tipo_jogo):
     janela_login.resizable(False, False)
 
     # adiciona os widgets da janela de login
-    label_usuario = Label(janela_login, font=('Montserrat', 20), text="Usuário:")
-    label_usuario.place(x=110, y=30)
-    entry_usuario = Entry(janela_login, font=('Montserrat', 20))
-    entry_usuario.place(x=260, y=30)
-    label_senha = Label(janela_login, font=('Montserrat', 20), text="Senha:")
-    label_senha.place(x=110, y=140)
-    entry_senha = Entry(janela_login, font=('Montserrat', 20),  show="*")
-    entry_senha.place(x=260, y=140)
-    botao_login = Button(janela_login, font=('Montserrat', 20),  text="Login", command=lambda: realizar_login(entry_usuario.get(), entry_senha.get(), tipo_jogo))
-    botao_login.place(x=210, y=190)
+    label_p1 = Label(janela_login, font=('Montserrat', 20), text="P1:")
+    label_p1.place(x=30, y=30)
+    entry_p1 = Entry(janela_login, font=('Montserrat', 20))
+    entry_p1.place(x=110, y=30)
+    label_p2 = Label(janela_login, font=('Montserrat', 20), text="P2:")
+    label_p2.place(x=30, y=80)
+    entry_p2 = Entry(janela_login, font=('Montserrat', 20))
+    entry_p2.place(x=110, y=80)
+    label_p3 = Label(janela_login, font=('Montserrat', 20), text="P3:")
+    label_p3.place(x=30, y=140)
+    entry_p3 = Entry(janela_login, font=('Montserrat', 20))
+    entry_p3.place(x=110, y=140)
+    label_p4 = Label(janela_login, font=('Montserrat', 20), text="P4:")
+    label_p4.place(x=30, y=190)
+    entry_p4 = Entry(janela_login, font=('Montserrat', 20))
+    entry_p4.place(x=110, y=190)
+    label_p5 = Label(janela_login, font=('Montserrat', 20), text="P5:")
+    label_p5.place(x=30, y=240)
+    entry_p5 = Entry(janela_login, font=('Montserrat', 20))
+    entry_p5.place(x=110, y=240)
+    label_p6 = Label(janela_login, font=('Montserrat', 20), text="P6:")
+    label_p6.place(x=30, y=300)
+    entry_p6 = Entry(janela_login, font=('Montserrat', 20))
+    entry_p6.place(x=110, y=300)
+    botao_login = Button(janela_login, font=('Montserrat', 20),  text="Entrar", command=lambda: dadosUSER(entry_p1.get(), entry_p2.get(),entry_p3.get(), entry_p4.get(),entry_p5.get(), entry_p6.get(), tipo_jogo))
+    botao_login.place(x=210, y=340)
     label_mensagem = Label(janela_login, font=('Montserrat', 20),  text="")
-    label_mensagem.place(x=210, y=360)
+    label_mensagem.place(x=400, y=340)
 
 # inicia o loop principal da janela
 janela_principal.mainloop()
-
-# define a função para realizar o login
-def realizar_login(usuario, senha, tipo_jogo):
-    # implemente sua lógica de login aqui
-    # ...
-    usuario = ...
-    senha = ...
-    tipo_jogo = ...
